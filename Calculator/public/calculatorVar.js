97066530a4e20e980f3cc76f2d0bb8068a6ef163
Ali Alkhulaqi <59796537+AliA749@users.noreply.github.com>
2023-11-16 12:44:21 -0500

    Add files via upload

---
+++ Calculator/public/calculatorVar.js
@@ -0,0 +1,11 @@
+window.addEventListener("load",addEventListener)
+
+function addEventListener()
+{
+	document.getElementById("btn1").addEventListener("click",inputOne);
+}
+
+function inputOne()
+{
+	alert("Hi")
+}
