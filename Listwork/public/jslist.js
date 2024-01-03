3a7460602a93605228a7c62996d13d4f8fc16471
Ali Alkhulaqi <59796537+AliA749@users.noreply.github.com>
2024-01-03 12:44:59 -0500

    Add files via upload

---
+++ Listwork/public/jslist.js
@@ -0,0 +1,12 @@
+window.addEventListener("load",Initialize);
+
+function Initialize()
+{
+	let datalist=["ford","GMC","BMW","Mercedes Benz"];
+	let list=document.getElementById("mylist");
+	
+	for(i=0;i<=datalist.length-1;++i)
+	{
+		var li=document.createElement
+	}
+}
