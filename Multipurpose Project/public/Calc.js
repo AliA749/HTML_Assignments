4bc7b98b9e39609ed31ba1ff5c4daba0129c2e7a
Ali Alkhulaqi <59796537+AliA749@users.noreply.github.com>
2024-02-12 12:17:13 -0500

    Add files via upload
    New and complete project edits will be done later

---
+++ Multipurpose Project/public/Calc.js
@@ -0,0 +1,307 @@
+window.addEventListener("load",AddListeners);
+
+let Equation;
+Equation = 0
+let IP1;
+IP1 = "";
+let IP2;
+IP2 = "";
+var Sign;
+Sign = "";
+let Process;
+
+	
+function AddListeners()
+{
+	Keys();
+	document.getElementById("btn1").addEventListener("click", One);
+	document.getElementById("btn2").addEventListener("click", Two);
+	document.getElementById("btn3").addEventListener("click", Three);
+	document.getElementById("btn4").addEventListener("click", Four);
+	document.getElementById("btn5").addEventListener("click", Five);
+	document.getElementById("btn6").addEventListener("click", Six);
+	document.getElementById("btn7").addEventListener("click", Seven);
+	document.getElementById("btn8").addEventListener("click", Eight);
+	document.getElementById("btn9").addEventListener("click", Nine);
+	document.getElementById("btn0").addEventListener("click", Zero);
+	document.getElementById("btnMult").addEventListener("click", Multiply);
+	document.getElementById("btnDiv").addEventListener("click", Divide);
+	document.getElementById("btnSub").addEventListener("click", Minus);
+	document.getElementById("btnAdd").addEventListener("click", Add);
+	document.getElementById("btnDec").addEventListener("click", Decimal);
+	document.getElementById("btnNeg").addEventListener("click", Negative);
+	document.getElementById("btnP").addEventListener("click", Percent);	
+	document.getElementById("btnexpo").addEventListener("click", Exponent);
+	document.getElementById("btnSIN").addEventListener("click", Sin);
+	document.getElementById("btnCOS").addEventListener("click", Cos);
+	document.getElementById("btnTAN").addEventListener("click", Tan);
+	document.getElementById("btnSqroot").addEventListener("click", Squareroot);		
+	document.getElementById("btnC").addEventListener("click", Clear);
+	document.getElementById("btnEqu").addEventListener("click", Result);
+	document.getElementById("mainpage").addEventListener("click",mainpage);															
+}
+
+function Keys()
+{
+	if (event.key == 1){One()}
+	if (event.key == 2){Two()}
+	if (event.key == 3){Three()}
+	if (event.key == 4){Four()}
+	if (event.key == 5){Five()}
+	if (event.key == 6){Six()}
+	if (event.key == 7){Seven()}
+	if (event.key == 8){Eight()}
+	if (event.key == 9){Nine()}
+	if (event.key == 0){Zero()}
+	
+}
+
+function Clear()
+{
+	Process = ""
+	IP1 = ""
+	IP2 = ""
+	Sign = ""
+	InBr = ""
+	Change()
+}
+
+function One()
+{
+	IP2 = IP2 + "1";
+	Change()
+}
+
+function Two()
+{
+	IP2 = IP2 + "2";
+	Change()
+}
+
+function Three()
+{
+	IP2 = IP2 + "3";
+	Change()
+}
+
+function Four()
+{
+	IP2 = IP2 + "4";
+	Change()
+}
+
+function Five()
+{
+	IP2 = IP2 + "5";
+	Change()
+}
+
+function Six()
+{
+	IP2 = IP2 + "6";
+	Change()
+}
+
+function Seven()
+{
+	IP2 = IP2 + "7";
+	Change()
+}
+
+function Eight()
+{
+	IP2 = IP2 + "8";
+	Change()
+}
+
+function Nine()
+{
+	IP2 = IP2 + "9";
+	Change()
+}
+function Zero()
+{
+	IP2 = IP2 + "0";
+	Change()
+}
+
+function Add()
+{
+	if (Sign > "" && IP1 > "" || Process > "")
+		Result()
+	else
+	IP1 = IP2
+	IP2 = ""
+	Sign = "+"
+	Change()
+}
+
+function Minus()
+{
+	if (Sign > "" && IP2 > "" || Process > "")
+		Result()
+	else
+	IP1 = IP2
+	IP2 = ""
+	Sign = "-"
+	Change()
+}
+
+function Multiply()
+{
+	if (Sign > "" && IP1 > "" || Process > "")
+		Result()
+	else
+	IP1 = IP2
+	IP2 = ""
+	Sign = "x"
+	Change()	
+}
+
+function Divide()
+{
+	if (Sign > "" && IP1 > "" || Process > "")
+		Result()
+	else
+		IP1 = IP2
+		IP2 = ""
+		Sign = "/"
+		Change()
+}
+
+function Negative()
+{
+	if (IP2.includes("-") == true)
+		IP2 = IP2.replace("-","")
+	else
+		IP2 =  "-" + IP2
+		Change()
+}
+
+function Decimal()
+{
+	IP2 = IP2 + "."
+	Change()
+}
+
+function Percent()
+{
+	IP2 = (parseInt(IP2) / 100).toString()
+	Change()
+}
+
+function Exponent() {
+	if (IP1 !== "") {
+		IP1 = parseFloat(IP1) * parseFloat(IP1);
+	} else if (IP2 !== "") {
+		IP2 = parseFloat(IP2) * parseFloat(IP2);
+	}
+	else{
+		alert("Error");
+	}
+	Sign = "^";
+	Change();
+}
+
+
+
+function Squareroot() {
+	if(IP1 !="")
+	{
+		IP1=(parseInt(IP1)**0.5);
+	}
+	else if(IP2 !="")
+	{
+		IP2 =(parseInt(IP2) ** 0.5);
+	}
+	else {
+		alert("Error");
+	}
+	Sign="√";
+    Change();
+}
+
+function Sin()
+{
+	if(IP1 !="")
+	{
+		IP1=(parseFloat (Math.sin(IP1).toFixed(2)));
+	}
+	else if(IP2 !="")
+	{
+		IP2=(parseFloat (Math.sin(IP2).toFixed(2)));
+	}
+	else {
+		alert("Error");
+	}
+	Sign="SIN()";
+	Change();
+}
+
+function Cos()
+{
+	if(IP1 !="")
+	{
+		IP1=(parseFloat (Math.cos(IP1).toFixed(2)));
+	}
+	else if(IP2 !="")
+	{
+		IP2=(parseFloat (Math.cos(IP2).toFixed(2)));
+	}
+	else {
+		alert("Error");
+	}
+	Sign="COS()";
+	Change();
+}
+
+function Tan()
+{
+	if(IP1 !="")
+	{
+		IP1=(parseFloat (Math.tan(IP1).toFixed(2)));
+	}
+	else if(IP2 !="")
+	{
+		IP2=(parseFloat (Math.tan(IP2).toFixed(2)));
+	}
+	else {
+		alert("Error");
+	}
+	Sign="TAN()";
+	Change();
+}
+
+function Result()
+{	
+	Calc()
+	IP1 = Process.toString()
+	Sign = ""
+	IP2 = ""
+	Change()
+}
+
+function Change()
+{
+	document.getElementById("screen").innerHTML = IP1 +""+ Sign +""+ IP2 + InBr;
+}
+
+function Calc()
+{
+		if (Sign == "+")
+		{Process = parseFloat(IP1) + parseFloat(IP2)}
+		else if (Sign == "-")
+		{Process = parseFloat(IP1) - parseFloat(IP2)}
+		else if (Sign == "x")
+		{Process = parseFloat(IP1) * parseFloat(IP2)}
+		else if (Sign == "/")
+		{Process = parseFloat(IP1) / parseFloat(IP2)}
+		else {
+		alert("Error");
+	}	
+}
+
+function mainpage()
+{
+	document.getElementById("mainpage").onclick=window.location.assign("file:///C:/Users/alial/eclipse-workspace/Multipurpose%20Project/public/index.html");
+}
---
+++ Multipurpose Project/public/cardmatchjs.js
@@ -0,0 +1,244 @@
+window.addEventListener("load", AddListeners);
+window.addEventListener("load", Start);
+
+function Start()
+{
+	document.getElementById("Easy").style.display="none";
+	document.getElementById("Medium").style.display="none";
+	document.getElementById("Hard").style.display="none";
+	document.getElementById("SubmitbtnEZ").disabled=true;
+	document.getElementById("SubmitbtnM").disabled=true;
+	document.getElementById("SubmitbtnM").style.display="none";
+	document.getElementById("SubmitbtnH").disabled=true;
+	document.getElementById("SubmitbtnH").style.display="none";
+}
+function AddListeners() {
+document.getElementById("cmbdifficulty").addEventListener("change",choices);
+document.getElementById("SubmitbtnEZ").addEventListener("click",answersEZ);
+document.getElementById("SubmitbtnM").addEventListener("click",answerM);
+document.getElementById("SubmitbtnH").addEventListener("click",answerH);
+document.getElementById("NextPage").addEventListener("click",Nexpage);
+document.getElementById("GoBack").addEventListener("click", GoBack);
+}
+
+function choices()
+{
+	var questions=document.getElementById("cmbdifficulty").value;
+	if(questions=="Easy")
+	{
+		document.getElementById("Displaylbl").style.display="block";
+		document.getElementById("Answer1M").textContent="";
+		document.getElementById("Answer2M").textContent="";
+		 document.getElementById("Answer3M").textContent="";
+		 document.getElementById("Answer1H").textContent="";
+		document.getElementById("Answer2H").textContent="";
+		 document.getElementById("Answer3H").textContent="";
+		document.getElementById("Easy").style.display="block";
+		document.getElementById("Medium").style.display="none";
+		document.getElementById("Hard").style.display="none";
+		document.getElementById("SubmitbtnEZ").disabled=false;
+	}
+	else if(questions=="Medium")
+	{
+		document.getElementById("Displaylbl2").style.display="block";
+		document.getElementById("Answer1EZ").textContent="";
+		document.getElementById("Answer2EZ").textContent="";
+		 document.getElementById("Answer3EZ").textContent="";
+		 document.getElementById("Answer1H").textContent="";
+		document.getElementById("Answer2H").textContent="";
+		 document.getElementById("Answer3H").textContent="";
+		document.getElementById("Easy").style.display="none";
+		document.getElementById("Medium").style.display="block";
+		document.getElementById("SubmitbtnM").disabled=false;
+		document.getElementById("SubmitbtnEZ").style.display="none";
+		document.getElementById("SubmitbtnH").style.display="none";
+		document.getElementById("SubmitbtnM").style.display="block";
+	}
+	else if(questions=="Hard")
+	{
+		document.getElementById("Displaylbl3").style.display="block";
+		document.getElementById("Answer1EZ").textContent="";
+		document.getElementById("Answer2EZ").textContent="";
+		 document.getElementById("Answer3EZ").textContent="";
+		 document.getElementById("Answer1M").textContent="";
+		document.getElementById("Answer2M").textContent="";
+		 document.getElementById("Answer3M").textContent="";
+		document.getElementById("Hard").style.display="block";
+		document.getElementById("Medium").style.display="none";
+		document.getElementById("Easy").style.display="none";
+		document.getElementById("SubmitbtnH").disabled=false;
+		document.getElementById("SubmitbtnEZ").style.display="none";
+		document.getElementById("SubmitbtnM").style.display="none";
+		document.getElementById("SubmitbtnH").style.display="block";
+	}
+	else{
+		alert("Choose an difficulty");
+		
+	}
+	
+}
+
+function answersEZ() {
+	document.getElementById("Answer1M").value="";
+	document.getElementById("Answer2M").value="";
+	document.getElementById("Answer3M").value="";
+	document.getElementById("Answer1H").value="";
+	document.getElementById("Answer2H").value="";
+	document.getElementById("Answer3H").value="";
+    var answer1ez = document.getElementById("Answer1EZ").value;
+    var answer2ez = document.getElementById("Answer2EZ").value;
+    var answer3ez = document.getElementById("Answer3EZ").value;
+    var life1 = document.getElementById("Card1");
+    var life2 = document.getElementById("Card2");
+    var life3 = document.getElementById("Card3");
+
+    switch (answer1ez) {
+        case "Hypertext Markup Language":
+            alert("correct!!");
+            break;
+        default:
+            alert("WRONG, The answer:Hypertext Markup Language");
+            document.getElementById("Card1").style.display = "none";
+    }
+
+    switch (answer2ez) {
+        case "75":
+            alert("correct2!!");
+            break;
+        default:
+            alert("WRONG2, The answer:75");
+            document.getElementById("Card2").style.display = "none";
+    }
+
+    switch (answer3ez) {
+        case "Elon Musk":
+            alert("correct3!!");
+            break;
+        default:
+            alert("WRONG3, The answer:Elon Musk");
+            document.getElementById("Card3").style.display = "none";
+    }
+
+	if (life1.style.display=="none" && life2.style.display=="none" && life3.style.display=="none")
+	{
+		alert("GAME OVER!!!");
+		window.location.reload();
+	}
+
+}
+function answerM(){
+	document.getElementById("Answer1EZ").value="";
+	document.getElementById("Answer2EZ").value="";
+	document.getElementById("Answer3EZ").value="";
+	document.getElementById("Answer1H").value="";
+	document.getElementById("Answer2H").value="";
+	document.getElementById("Answer3H").value="";
+	var answer1M=document.getElementById("Answer1M").value;
+	var answer2M=document.getElementById("Answer2M").value;
+	var answer3M=document.getElementById("Answer3M").value;
+	var life1=document.getElementById("Card1");
+	var life2=document.getElementById("Card2");
+	var life3=document.getElementById("Card3");
+	  switch (answer1M) {
+        case "Shows Element":
+            alert("correct!!");
+            break;
+        default:
+            alert("Incorrect Answer:Shows Element");
+            document.getElementById("Card1").style.display = "none";
+            document.getElementById("Card2").style.display = "none";
+    }
+
+    switch (answer2M) {
+        case "5":
+            alert("correct2!!");
+            break;
+        default:
+            alert("WRONG#2 answer:5");
+            document.getElementById("Card2").style.display = "none";
+            document.getElementById("Card3").style.display = "none";
+    }
+
+    switch (answer3M) {
+        case "Steve Jobs":
+            alert("correct3!!");
+            break;
+        default:
+            alert("WRONG#3 Answer:Steve Jobs");
+            document.getElementById("Card3").style.display = "none";
+            document.getElementById("Card1").style.display = "none";
+    }
+	if (life1.style.display=="none" && life2.style.display=="none" && life3.style.display=="none")
+	{
+		alert("GAME OVER!!!");
+		window.location.reload();
+	}
+	else{
+		alert("You Still have Lives nice!");
+	}
+}
+
+function answerH()
+{
+	document.getElementById("Answer1M").value="";
+	document.getElementById("Answer2M").value="";
+	document.getElementById("Answer3M").value="";
+	document.getElementById("Answer1EZ").value="";
+	document.getElementById("Answer2EZ").value="";
+	document.getElementById("Answer3EZ").value="";
+	var answer1H=document.getElementById("Answer1H").value;
+	var answer2H=document.getElementById("Answer2H").value;
+	var answer3H=document.getElementById("Answer3H").value;
+	var life1=document.getElementById("Card1");
+	var life2=document.getElementById("Card2");
+	var life3=document.getElementById("Card3");
+	  switch (answer1H) {
+        case "Initialize Elements":
+            alert("correct answer!");
+            break;
+        default:
+            alert("Incorrect Answer:Initilize Elements");
+            document.getElementById("Card1").style.display = "none";
+            document.getElementById("Card2").style.display = "none";
+            document.getElementById("Card3").style.display = "none";
+    }
+
+    switch (answer2H) {
+        case "25":
+            alert("correct#2!");
+            break;
+        default:
+            alert("WRONG#2 answer:25");
+            document.getElementById("Card1").style.display = "none";
+            document.getElementById("Card2").style.display = "none";
+            document.getElementById("Card3").style.display = "none";
+    }
+
+    switch (answer3H) {
+        case "Charles Babbage":
+            alert("correct3!!");
+            break;
+        default:
+            alert("WRONG#3 Answer:Charles Babbage");
+            document.getElementById("Card1").style.display = "none";
+            document.getElementById("Card2").style.display = "none";
+            document.getElementById("Card3").style.display = "none";
+    }
+	if (life1.style.display=="none" && life2.style.display=="none" && life3.style.display=="none")
+	{
+		alert("GAME OVER!!!");
+		window.location.reload();
+	}
+	else{
+		alert("You Still have Lives nice!");
+	}
+}
+function Nexpage()
+{
+	document.getElementById("NextPage").onclick=window.location.assign("file:///C:/Users/alial/eclipse-workspace/Multipurpose%20Project/public/ResearchEngine.html");
+}
+
+function GoBack()
+{
+	document.getElementById("GoBack").onclick=window.location.assign("file:///C:/Users/alial/eclipse-workspace/Multipurpose%20Project/public/index.html");
+}
